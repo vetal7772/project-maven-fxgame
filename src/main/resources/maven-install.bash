385542f138ddf42ef90bddaea2e8bd7fa6b8e287
vit_s <vit_shl@ukr.net>
2024-02-11 16:07:09 +0200

    add .jar and  .bash(with run-command) to the folder resources

---
+++ src/main/resources/maven-install.bash
@@ -0,0 +1,10 @@
+mvn install:install-file -Dfile=I:\JAVA\JavaRushUniver30_01_2023\project-maven-fxgame\lib\desktop-game-engine.jar -DgroupId=com.javarush -DartifactId=desktop-game-engine -Dversion=1.0 -Dpackaging=jar
+
+
+Run from `command`line
+
+C:\Users\vit_s\.jdks\corretto-18.0.2\bin\java.exe -jar "I:\JAVA\JavaRushUniver30_01_2023\Maven\project-maven-master\project-maven-master\target\project-maven-1.2.jar"
+
+2 edition of pom.xml
+
+C:\Users\vit_s\.jdks\corretto-18.0.2\bin\java.exe -jar "I:\JAVA\JavaRushUniver30_01_2023\project-maven-fxgame\target\project-maven-1.2.jar"
